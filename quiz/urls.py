21c837980d942e1399b2951c2d4a0f5609856118
Jeongseo Han <jeongseo21@gmail.com>
2020-08-21 11:48:47 +0900

    Ready for deploy

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path("hello/", helloAPI),
     path("<int:id>/", randomQuiz),
 ]
+
+#배포관련 설치해야하는 파일
+# django-cors-headers : cors에러 방지
+# gunicorn : 배포를 위한 도구
+# psycopg2-binary : postgresql 사용을 위한 패키지
+# whitenoise : 정적파일의 사용을 돕는 미들웨어
+# dj-database-url : postgresql 사용을 위한 패키지
